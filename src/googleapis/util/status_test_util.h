27fc19628936fce117969df1a4b23232621c7249
Tony Aiuto <aiuto@google.com>
2015-06-22 10:21:43 -0400

    add mising file status_test_util.h

---
+++ src/googleapis/util/status_test_util.h
@@ -0,0 +1,12 @@
+#ifndef GOOGLEAPIS_UTIL_STATUS_TEST_UTIL_H__
+#define GOOGLEAPIS_UTIL_STATUS_TEST_UTIL_H__
+
+// #include "testing/base/public/gunit.h"
+#include "googleapis/util/status.h"
+
+// Macros for testing the results of functions that return util::Status.
+
+#define EXPECT_OK(statement) EXPECT_EQ(googleapis::util::Status::OK, (statement))
+#define ASSERT_OK(statement) ASSERT_EQ(googleapis::util::Status::OK, (statement))
+
+#endif  // GOOGLEAPIS_UTIL_STATUS_TEST_UTIL_H__
