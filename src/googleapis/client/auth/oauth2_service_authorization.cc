b7783e339789f3c35c61650578f6d22e87c01255
Tony Aiuto <aiuto@google.com>
2016-03-18 14:03:03 -0400

    move JWT code to distinct module

--- src/googleapis/client/auth/oauth2_service_authorization.cc
+++ src/googleapis/client/auth/oauth2_service_authorization.cc
@@ -21,6 +21,7 @@
 #include <memory>
 
 #include "googleapis/config.h"
+#include "googleapis/client/auth/jwt_builder.h"
 #include "googleapis/client/auth/oauth2_service_authorization.h"
 
 #include "googleapis/client/data/data_reader.h"
@@ -48,132 +49,6 @@
 namespace googleapis {
 
 namespace client {
-
-/*
- * A helper class used to create the JWT we pass to the OAuth2.0 server.
- *
- * This class could be broken out into its own module if it is needed elsewhere
- * or to test more explicitly.
- */
-class JwtBuilder {
- public:
-  static googleapis::util::Status LoadPrivateKeyFromPkcs12Path(
-      const string& path, string* result_key) {
-    googleapis::util::Status status;
-    result_key->clear();
-    OpenSSL_add_all_algorithms();
-    BIO* bio = BIO_new(BIO_s_mem());
-    EVP_PKEY* pkey = LoadPkeyFromP12Path(path.c_str());
-    if (!pkey) {
-      status = StatusUnknown(
-          StrCat("OpenSSL failed parsing PKCS#12 error=", ERR_get_error()));
-    } else if (PEM_write_bio_PrivateKey(
-        bio, pkey, NULL, NULL, 0, NULL, NULL) < 0) {
-      status = StatusUnknown("OpenSSL Failed writing BIO memory output");
-    }
-
-    if (status.ok()) {
-      BUF_MEM *mem_ptr = NULL;
-      BIO_get_mem_ptr(bio, &mem_ptr);
-      result_key->assign(mem_ptr->data, mem_ptr->length);  // copies data out
-    }
-
-    BIO_free(bio);
-    EVP_PKEY_free(pkey);
-    return status;
-  }
-
-
-  static void AppendAsBase64(const char* data, size_t size, string* to) {
-    string encoded;
-    strings::WebSafeBase64Escape(
-        reinterpret_cast<const unsigned char*>(data), size, &encoded, false);
-    to->append(encoded);
-  }
-
-  static void AppendAsBase64(const string& from, string* to) {
-    AppendAsBase64(from.data(), from.size(), to);
-  }
-
-  static EVP_PKEY* LoadPkeyFromData(const StringPiece& data) {
-    BIO* bio = BIO_new_mem_buf(const_cast<char*>(data.data()), data.size());
-    EVP_PKEY* pkey =
-        PEM_read_bio_PrivateKey(bio, NULL, 0, const_cast<char*>("notasecret"));
-    if (pkey == NULL) {
-      char buffer[128];
-      ERR_error_string(ERR_get_error(), buffer);
-      LOG(ERROR) << "OpenSslError reading private key: " << buffer;
-    }
-    BIO_free(bio);
-    return pkey;
-  }
-
-  static EVP_PKEY* LoadPkeyFromP12Path(const char* pkcs12_key_path) {
-    //    OpenSSL_add_all_algorithms();
-    X509 *cert = NULL;
-    STACK_OF(X509) *ca = NULL;
-    PKCS12 *p12;
-
-    FILE* fp = fopen(pkcs12_key_path, "rb");
-    CHECK(fp != NULL);
-    p12 = d2i_PKCS12_fp(fp, NULL);
-    fclose(fp);
-    if (!p12) {
-      googleapis::util::Status status = StatusUnknown(
-          StrCat("OpenSSL failed reading PKCS#12 error=", ERR_get_error()));
-      LOG(ERROR) << status.error_message();
-      return NULL;
-    }
-
-    EVP_PKEY* pkey = NULL;
-    int ok = PKCS12_parse(p12, "notasecret", &pkey, &cert, &ca);
-    PKCS12_free(p12);
-    if (cert) {
-      X509_free(cert);
-    }
-    if (ca) {
-      sk_X509_pop_free(ca, X509_free);
-    }
-
-    CHECK(ok);
-    return pkey;
-  }
-
-  static googleapis::util::Status MakeJwtUsingEvp(
-      const string& claims, EVP_PKEY* pkey, string* jwt) {
-    const char* plain_header = "{\"alg\":\"RS256\",\"typ\":\"JWT\"}";
-    string data_to_sign;
-    AppendAsBase64(plain_header, &data_to_sign);
-    data_to_sign.append(".");
-    AppendAsBase64(claims, &data_to_sign);
-
-    googleapis::util::Status status;
-    EVP_MD_CTX ctx;
-    EVP_SignInit(&ctx, EVP_sha256());
-    EVP_SignUpdate(&ctx, data_to_sign.c_str(), data_to_sign.size());
-
-    unsigned int buffer_size = EVP_PKEY_size(pkey);
-    std::unique_ptr<char[]> buffer(new char[buffer_size]);
-
-    if (EVP_SignFinal(
-            &ctx,
-            reinterpret_cast<unsigned char*>(buffer.get()),
-            &buffer_size,
-            pkey) == 0) {
-      status = StatusInternalError(
-          StrCat("Failed signing JWT. error=", ERR_get_error()));
-    }
-
-    EVP_MD_CTX_cleanup(&ctx);
-
-    if (!status.ok()) return status;
-
-    jwt->swap(data_to_sign);
-    jwt->append(".");
-    AppendAsBase64(buffer.get(), buffer_size, jwt);
-    return StatusOk();
-  }
-};
 
 OAuth2ServiceAccountFlow::OAuth2ServiceAccountFlow(
     HttpTransport* transport)
